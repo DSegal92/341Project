b52145574913cc534c9fc36e9ec398045b984790
Daniel Segal <DSegal92@gmail.com>
2013-04-12 13:23:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediaLibrary::Application.config.secret_token = 'dc470eac0b3eee42f6cd38c98980d4005b6f4e155f98c641b083a9b3db28e77785789b1fddca3256f5b370df3f3c48f6beed043f49eb72a56917dbb22ad6869b'
